9c61b66174ac324364ceeb3654330898f6eaf6e7
ogola <ogolajeremy601@gmail.com>
2024-05-14 15:13:09 +0300

    adds programmatic nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, Navigate } from "react-router-dom";
+import { useState, useEffect} from "react"
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  const login = () => {
+    setIsLoggedIn(true);
+  }
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  }
+
+  useEffect(()=>{
+    if(isLoggedIn){
+      navigate("/");
+    }else{
+      navigate("/login");
+    }
+  },[isLoggedIn])
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+         {isLoggedIn ? <NavBar logout={logout}  /> : <Navigate to="/login" />}
+      <Outlet context={login}/>
     </div>
   );
 }
